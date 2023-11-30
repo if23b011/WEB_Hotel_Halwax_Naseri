ca2ebb5fe511d728bf159db616f5c2cc6267ab49
if23b011 <145259777+if23b011@users.noreply.github.com>
2023-11-30 14:19:55 +0100

    mobile friendly FERTIG

--- sites/newsupload.php
+++ sites/newsupload.php
@@ -10,8 +10,8 @@ session_start();
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Hotel Tropicana - Newsupload</title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-            integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="../css/style.css">
 </head>
 
@@ -21,30 +21,26 @@ session_start();
     <!-- Content-->
 
     <div class="container" style="margin-bottom: 100px;">
-        <h1>Hotel Tropicana - News uploaden</h1>
-        <form method="post" action="../utils/upload.php" enctype="multipart/form-data">
-            <div class="container">
-                <div class="d-grid col-6 mx-auto">
-                    <div class="mb-3">
-                        <label for="exampleFormControlText" class="form-label">
-                            <p>Newstitel:</p>
-                        </label>
-                        <input class="form-control" type="text" name="title" id="title">
-                    </div>
-                    <div class="mb-3">
-                        <label for="exampleFormControlTextarea1" class="form-label">
-                            <p>Newstext:</p>
-                        </label>
-                        <textarea class="form-control" id="text" rows="3" name="text"></textarea>
-                        <p>Bild auswählen:</p>
-                        <p><input type="file" name="fileToUpload" id="fileToUpload"></p>
-                    </div>
-                    <div class="d-grid gap-2">
-                        <input class="btn btn-primary" type="submit" value="Submit">
-                    </div>
+        <h1>News uploaden</h1>
+        <div class="container">
+            <form method="post" action="../utils/upload.php" enctype="multipart/form-data">
+                <div class="d-grid gap-3 col-md-6 mx-auto mb-4">
+                    <label for="exampleFormControlText" class="form-label">
+                        <p>Newstitel:</p>
+                    </label>
+                    <input class="form-control" type="text" name="title" id="title">
+                    <label for="exampleFormControlTextarea1" class="form-label">
+                        <p>Newstext:</p>
+                    </label>
+                    <textarea class="form-control" id="text" rows="3" name="text"></textarea>
+                    <p>Bild auswählen:</p>
+                    <p><input type="file" name="fileToUpload" id="fileToUpload"></p>
                 </div>
-            </div>
-        </form>
+                <div class="d-grid gap-2">
+                    <input class="btn btn-primary" type="submit" value="Submit">
+                </div>
+            </form>
+        </div>
     </div>
     <!-- Footer-->
     <?php include '../utils/footer.php'; ?>
