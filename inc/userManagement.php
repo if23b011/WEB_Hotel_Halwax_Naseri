c728ddb8c71e976ba4d812a2570f73b31820bbd0
if23b011 <145259777+if23b011@users.noreply.github.com>
2023-12-26 16:50:34 +0100

    Update userManagement.php

--- inc/userManagement.php
+++ inc/userManagement.php
@@ -3,7 +3,7 @@
     $userId = $gender = $firstname = $lastname = $email = $date = $type = $active = "";
     require_once 'utils/dbaccess.php';
     require_once 'utils/functions.php';
-    $sql = "SELECT * FROM users WHERE type = 'user'";
+    $sql = "SELECT * FROM users";
     $result = $conn->query($sql);
     if ($result->num_rows > 0) {
         ?>
@@ -14,7 +14,6 @@
                         <h1>User</h1>
                     </a>
                     <?php
-                    //FIXME: ALLE HEIßEN ARMIN NASERI
                     while ($row = $result->fetch_assoc()) {
                         $userId = input($row["userId"]);
                         $gender = input($row["gender"]);
@@ -25,86 +24,75 @@
                         $birthDate = date("Y-m-d", strtotime($date));
                         $type = input($row["type"]);
                         $active = input($row["active"]);
-                        if ($_SERVER["REQUEST_METHOD"] == "POST") {
-                            if (isset($_POST["gender"])) {
-                                $gender = input($_POST["gender"]);
-                            }
-                            if (isset($_POST["firstname"])) {
-                                $firstname = input($_POST["firstname"]);
-                            }
-                            if (isset($_POST["lastname"])) {
-                                $lastname = input($_POST["lastname"]);
-                            }
-                            if (isset($_POST["date"])) {
-                                $date = input($_POST["date"]);
-                            }
+                    }
+                    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                        if (isset($_POST["userId"])) {
+                            $userId = input($_POST["userId"]);
+                            $gender = isset($_POST["gender"]) ? input($_POST["gender"]) : $gender;
+                            $firstname = isset($_POST["firstname"]) ? input($_POST["firstname"]) : $firstname;
+                            $lastname = isset($_POST["lastname"]) ? input($_POST["lastname"]) : $lastname;
+                            $date = isset($_POST["date"]) ? input($_POST["date"]) : $date;
                             $birthDate = date("Y-m-d", strtotime($date));
-                            if (isset($_POST["type"])) {
-                                $type = input($_POST["type"]);
-                            }
-                            if (isset($_POST["active"])) {
-                                $active = input($_POST["active"]);
-                            }
+                            $type = isset($_POST["type"]) ? input($_POST["type"]) : $type;
+                            $active = isset($_POST["active"]) ? input($_POST["active"]) : $active;
+
                             $sql = "UPDATE users SET gender = ?, firstname = ? , lastname = ? , birthdate = ? , type = ? , active = ? WHERE userId = ?";
                             $stmt = mysqli_stmt_init($conn);
-                            if (!mysqli_stmt_prepare($stmt, $sql)) { ?>
-                                <p>SQL-Fehler</p>
-                                <?php return;
+                            if (mysqli_stmt_prepare($stmt, $sql)) {
+                                mysqli_stmt_bind_param($stmt, "sssssii", $gender, $firstname, $lastname, $birthDate, $type, $active, $userId);
+                                mysqli_stmt_execute($stmt);
                             }
-                            mysqli_stmt_bind_param($stmt, "sssssss", $gender, $firstname, $lastname, $birthDate, $type, $active, $userId);
-                            mysqli_stmt_execute($stmt);
-                            mysqli_stmt_close($stmt);
                         }
-                        ?>
-                        <p></p>
-                        <a>
-                            <span></span>
-                            <h2>
-                                <?php
-                                if ($row["gender"] == "M") {
-                                    $genderWritten = "Herr ";
-                                } else {
-                                    $genderWritten = "Frau ";
-                                }
-                                echo $genderWritten . " " . $row["firstname"] . " " . $row["lastname"];
-                                ?>
-                            </h2>
-                        </a>
-                        <div class="user-box">
-                            <input type="text" name="userId" value="<?php echo $userId ?>" disabled>
-                            <label>UserID</label>
-                        </div>
-                        <div class="user-box">
-                            <input type="text" name="gender" value="<?php echo $gender ?>">
-                            <label>Geschlecht (M=männlich, W=weiblich)</label>
-                        </div>
-                        <div class="user-box">
-                            <input type="text" name="firstname" value="<?php echo $firstname ?>">
-                            <label>Vorname</label>
-                        </div>
-                        <div class="user-box">
-                            <input type="text" name="lastname" value="<?php echo $lastname ?>">
-                            <label>Nachname</label>
-                        </div>
-                        <div class="user-box">
-                            <input type="text" name="email" value="<?php echo $email ?>">
-                            <label>E-Mail</label>
-                        </div>
-                        <div class="user-box">
-                            <input type="date" name="birthdate" value="<?php echo $date ?>">
-                            <label>Geburtstag</label>
-                        </div>
-                        <div class="user-box">
-                            <input type="text" name="type" value="<?php echo $type ?>">
-                            <label>Type</label>
-                        </div>
-                        <div class="user-box">
-                            <input type="text" name="type" value="<?php echo $active ?>">
-                            <label>Account aktiv (1=aktiv, 0 = inkativ)</label>
-                        </div>
-                        <input type="submit" value="Änderungen übernehmen" class="loginBoxSubmit">
-                        <?php
                     }
+                    ?>
+                    <p></p>
+                    <a>
+                        <span></span>
+                        <h2>
+                            <?php
+                            if ($row["gender"] == "M") {
+                                $genderWritten = "Herr ";
+                            } else {
+                                $genderWritten = "Frau ";
+                            }
+                            echo $genderWritten . " " . $row["firstname"] . " " . $row["lastname"];
+                            ?>
+                        </h2>
+                    </a>
+                    <div class="user-box">
+                        <input type="text" name="userId" value="<?php echo $userId ?>" disabled>
+                        <label>UserID</label>
+                    </div>
+                    <div class="user-box">
+                        <input type="text" name="gender" value="<?php echo $gender ?>">
+                        <label>Geschlecht (M=männlich, W=weiblich)</label>
+                    </div>
+                    <div class="user-box">
+                        <input type="text" name="firstname" value="<?php echo $firstname ?>">
+                        <label>Vorname</label>
+                    </div>
+                    <div class="user-box">
+                        <input type="text" name="lastname" value="<?php echo $lastname ?>">
+                        <label>Nachname</label>
+                    </div>
+                    <div class="user-box">
+                        <input type="text" name="email" value="<?php echo $email ?>">
+                        <label>E-Mail</label>
+                    </div>
+                    <div class="user-box">
+                        <input type="date" name="birthdate" value="<?php echo $date ?>">
+                        <label>Geburtstag</label>
+                    </div>
+                    <div class="user-box">
+                        <input type="text" name="type" value="<?php echo $type ?>">
+                        <label>Type</label>
+                    </div>
+                    <div class="user-box">
+                        <input type="text" name="type" value="<?php echo $active ?>">
+                        <label>Account aktiv (1=aktiv, 0 = inkativ)</label>
+                    </div>
+                    <input type="submit" value="Änderungen übernehmen" class="loginBoxSubmit">
+                    <?php
     } ?>
             </form>
         </div>
