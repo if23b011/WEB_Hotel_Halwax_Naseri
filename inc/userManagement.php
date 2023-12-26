ae3ae829a155c7c68d1b54eeabeecda8a2469cd4
if23b011 <145259777+if23b011@users.noreply.github.com>
2023-12-26 19:01:20 +0100

    Update userManagement.php

--- inc/userManagement.php
+++ inc/userManagement.php
@@ -23,8 +23,55 @@
                         $date = input($row["birthdate"]);
                         $birthDate = date("Y-m-d", strtotime($date));
                         $type = input($row["type"]);
-                        $active = input($row["active"]);
-                    }
+                        $active = input($row["active"]); ?>
+                        <p></p>
+                        <a>
+                            <span></span>
+                            <h2>
+                                <?php
+                                if ($row["gender"] == "M") {
+                                    $genderWritten = "Herr ";
+                                } else {
+                                    $genderWritten = "Frau ";
+                                }
+                                echo $genderWritten . " " . $row["firstname"] . " " . $row["lastname"];
+                                ?>
+                            </h2>
+                        </a>
+                        <div class="user-box">
+                            <input type="text" name="userId" value="<?php echo $userId ?>" disabled>
+                            <label>UserID</label>
+                        </div>
+                        <div class="user-box">
+                            <input type="text" name="gender" value="<?php echo $gender ?>">
+                            <label>Geschlecht (M=männlich, W=weiblich)</label>
+                        </div>
+                        <div class="user-box">
+                            <input type="text" name="firstname" value="<?php echo $firstname ?>">
+                            <label>Vorname</label>
+                        </div>
+                        <div class="user-box">
+                            <input type="text" name="lastname" value="<?php echo $lastname ?>">
+                            <label>Nachname</label>
+                        </div>
+                        <div class="user-box">
+                            <input type="text" name="email" value="<?php echo $email ?>">
+                            <label>E-Mail</label>
+                        </div>
+                        <div class="user-box">
+                            <input type="date" name="birthdate" value="<?php echo $date ?>">
+                            <label>Geburtstag</label>
+                        </div>
+                        <div class="user-box">
+                            <input type="text" name="type" value="<?php echo $type ?>">
+                            <label>Type</label>
+                        </div>
+                        <div class="user-box">
+                            <input type="text" name="active" value="<?php echo $active ?>">
+                            <label>Account aktiv (1=aktiv, 0 = inkativ)</label>
+                        </div>
+                        <input type="submit" value="Änderungen übernehmen" class="loginBoxSubmit">
+                    <?php }
                     if ($_SERVER["REQUEST_METHOD"] == "POST") {
                         if (isset($_POST["userId"])) {
                             $userId = input($_POST["userId"]);
@@ -44,55 +91,6 @@
                             }
                         }
                     }
-                    ?>
-                    <p></p>
-                    <a>
-                        <span></span>
-                        <h2>
-                            <?php
-                            if ($row["gender"] == "M") {
-                                $genderWritten = "Herr ";
-                            } else {
-                                $genderWritten = "Frau ";
-                            }
-                            echo $genderWritten . " " . $row["firstname"] . " " . $row["lastname"];
-                            ?>
-                        </h2>
-                    </a>
-                    <div class="user-box">
-                        <input type="text" name="userId" value="<?php echo $userId ?>" disabled>
-                        <label>UserID</label>
-                    </div>
-                    <div class="user-box">
-                        <input type="text" name="gender" value="<?php echo $gender ?>">
-                        <label>Geschlecht (M=männlich, W=weiblich)</label>
-                    </div>
-                    <div class="user-box">
-                        <input type="text" name="firstname" value="<?php echo $firstname ?>">
-                        <label>Vorname</label>
-                    </div>
-                    <div class="user-box">
-                        <input type="text" name="lastname" value="<?php echo $lastname ?>">
-                        <label>Nachname</label>
-                    </div>
-                    <div class="user-box">
-                        <input type="text" name="email" value="<?php echo $email ?>">
-                        <label>E-Mail</label>
-                    </div>
-                    <div class="user-box">
-                        <input type="date" name="birthdate" value="<?php echo $date ?>">
-                        <label>Geburtstag</label>
-                    </div>
-                    <div class="user-box">
-                        <input type="text" name="type" value="<?php echo $type ?>">
-                        <label>Type</label>
-                    </div>
-                    <div class="user-box">
-                        <input type="text" name="type" value="<?php echo $active ?>">
-                        <label>Account aktiv (1=aktiv, 0 = inkativ)</label>
-                    </div>
-                    <input type="submit" value="Änderungen übernehmen" class="loginBoxSubmit">
-                    <?php
     } ?>
             </form>
         </div>
